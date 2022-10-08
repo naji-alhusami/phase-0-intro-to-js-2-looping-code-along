4e88a5fe6c973158b6708c0f39b25cbbfe51c2fb
naji-alhusami <naji.alhusami@bahcesehir.edu.tr>
2022-10-08 22:52:05 +0300

    Completed assignemnt

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const names = ["Charlie", "Samip", "Ali"];
+const event = "birthday";
+const newNames = [];
+
+function writeCards(names, event) {
+  for (let i = 0; i < names.length; i++) {
+    newNames.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return newNames;
+}
+
+function countDown(coDown = 0) {
+  while (coDown >= 0) {
+    console.log(coDown--);
+  }
+}
